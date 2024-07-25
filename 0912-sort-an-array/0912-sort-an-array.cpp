6e0e1e6e1947adb548c60e5a5fb9e16c4c8fd9e9
Gowlapalli Rohit <102759922+ROHIT32767@users.noreply.github.com>
2024-07-25 11:30:09 +0530

    Time: 620 ms (21.37%), Space: 284.4 MB (20.10%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,56 +1,46 @@
 class Solution {
 public:
-    void merge_array(vector<int>& nums,int low,int mid,int high)
-    {
-        int size = high-low+1;
-        vector<int> res(size,-1);
-        int index = 0;
-        int index1 = low;
-        int index2 = mid+1;
-        while(index1<=mid && index2<=high)
-        {
-            if(nums[index1]<=nums[index2])
-            {
-                res[index] = nums[index1];
-                index++;
-                index1++;
-            }
-            else{
-                res[index] = nums[index2];
-                index++;
-                index2++;
-            }
-        }
-        while(index1<=mid)
-        {
-            res[index] = nums[index1];
-            index++;
+    
+    
+void merge_array(vector<int>& nums,int low,int mid,int high)
+{
+    int index1 = low;
+    int index2 = mid+1;
+    vector<int> res;
+    while(index1<=mid && index2<=high){
+        if(nums[index1]<nums[index2]){
+            res.push_back(nums[index1]);
             index1++;
         }
-        while(index2<=high)
-        {
-            res[index] = nums[index2];
-            index++;
+        else{
+            res.push_back(nums[index2]);
             index2++;
         }
-        for(int i=0;i<size;i++)
-        {
-            nums[i+low] = res[i];
-        }
     }
-    void merge(int start,int end,vector<int>& nums)
+    while(index1<=mid){
+        res.push_back(nums[index1]);
+        index1++;
+    }
+    while(index2<=high)
     {
-        if(start<end)
-        {
-            int mid = start + (end-start)/2;
-            merge(start,mid,nums);
-            merge(mid+1,end,nums);
-            merge_array(nums,start,mid,end);
-        }
-        return;
+        res.push_back(nums[index2]);
+        index2++;
     }
-    vector<int> sortArray(vector<int>& nums) {
+    for(int i=0;i<high-low+1;i++){
+        nums[i+low] = res[i];
+    }
+}
+
+void merge(int low,int high,vector<int>& nums){
+    int mid = low+(high-low)/2;
+    if(low<high){
+        merge(low,mid,nums);
+        merge(mid+1,high,nums);
+        merge_array(nums,low,mid,high);
+    }
+}
+vector<int> sortArray(vector<int>& nums) {
         merge(0,nums.size()-1,nums);
         return nums;
-    }
+}
 };
