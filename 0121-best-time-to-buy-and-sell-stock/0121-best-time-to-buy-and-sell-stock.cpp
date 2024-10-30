656d923a4ab76f8aee4dcae57d5e85e1f7c6a0b0
Gowlapalli Rohit <102759922+ROHIT32767@users.noreply.github.com>
2024-10-31 01:16:57 +0530

    Time: 2 ms (33.81%), Space: 100.2 MB (5.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    typedef long long INT;
-    INT max(INT A,INT B){
-        return A<B ? B: A;
-    }
-    INT min(INT A,INT B){
-        return A<B ? A:B;
-    }
     int maxProfit(vector<int>& prices) {
-        INT MIN = INT_MAX;
-        MIN = prices[0];
-        INT MAX = 0;
-        for(int i=1;i<prices.size();i++){
-            MAX = max(MAX,prices[i]-MIN);
-            MIN = min(MIN,prices[i]);
+        int size = prices.size();
+        vector<int> prefix_min(size,-1);
+        prefix_min[0] = prices[0];
+        for(int i=1;i<size;i++){
+            prefix_min[i] = min(prices[i],prefix_min[i-1]);
+        }
+        int MAX = 0;
+        for(int i=0;i<size;i++){
+            MAX = max(MAX,prices[i]-prefix_min[i]);
         }
         return MAX;
     }
